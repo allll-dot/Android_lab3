e76ffc4d80fe4ed0b600e11d244df46f5f713ae7
LastProgram <145275888+LastProgram@users.noreply.github.com>
2024-11-30 08:26:14 +0700

    Add read text listener

--- app/src/main/java/com/example/alab3/MainActivity.kt
+++ app/src/main/java/com/example/alab3/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.alab3
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,13 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val editText = findViewById<EditText>(R.id.edit_text)
+
+        editText.setOnEditorActionListener{_, _ ,_ ->
+            var input = editText.text.toString()
+            true
         }
     }
 }
