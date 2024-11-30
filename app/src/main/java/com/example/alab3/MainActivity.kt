a5b2a0dd0d4b92ba6a96fe6510d45c55294ff021
LastProgram <145275888+LastProgram@users.noreply.github.com>
2024-11-30 08:27:44 +0700

    Add output text

--- app/src/main/java/com/example/alab3/MainActivity.kt
+++ app/src/main/java/com/example/alab3/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.alab3
 
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,9 +14,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val editText = findViewById<EditText>(R.id.edit_text)
+        val textView = findViewById<TextView>(R.id.text_view)
 
         editText.setOnEditorActionListener{_, _ ,_ ->
             var input = editText.text.toString()
+            textView.text = ""
             true
         }
     }
